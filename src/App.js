e05396ba81da3929b96a8d577470e516ae28bc34
Patrik Přidal <98656826+vibemane@users.noreply.github.com>
2022-02-09 20:13:03 +0100

    based

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Post from "./Post";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username: "vibemane333",
+      caption: "ayyy",
+      imageUrl: "https://cdn.pixabay.com/photo/2016/04/04/14/12/monitor-1307227__480.jpg"
+    },
+    {
+      username: "vibemane333",
+      caption: "ayyy",
+      imageUrl: "https://cdn.pixabay.com/photo/2016/04/04/14/12/monitor-1307227__480.jpg"
+    }
+  ]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='app__header'>
+        <img
+          className='app__headerImage'
+          src='https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png'
+          alt=""
+        />
+      </div>
+
+      {
+        posts.map(post => (
+          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
+        ))
+      }     
+
     </div>
   );
 }
