3f0f0ed0dd5920da812c5302337133608b559984
Patrik Přidal <98656826+vibemane@users.noreply.github.com>
2022-02-10 20:29:16 +0100

    signup

--- src/App.js
+++ src/App.js
@@ -1,42 +1,144 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import Post from "./Post";
-import { db } from './Firebase';
+import { auth, db } from "./Firebase";
+import { Button, Input, makeStyles } from "@material-ui/core";
+import { Modal } from "@material-ui/core";
 
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const [modalStyle] = React.useState(getModalStyle);
   const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [user, setUser] = useState(null);
+  
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        //user has logged in...
+        console.log(authUser);
+        setUser(authUser);
+      
+      if (authUser.displayName) {
+        //don't update username
+      } else {
+        // if we just created acc
+        return authUser.updateProfile({
+          displayName: username,
+        });
+      }
+
+      } else {
+        //user has logged out...
+        setUser(null);
+      }
+    })
+    return () => {
+      // perform some cleanup actions
+      unsubscribe();
+    }
+  }, [user, username]);
 
   // useEffect -> runs a piece of code based on a specific condition
 
   useEffect(() => {
     //this is where code runing
-    db.collection('posts').onSnapshot(snapshot => {
-       // every time a new post is added, this code fire of
-       setPosts(snapshot.docs.map(doc => ({
+    db.collection("posts").onSnapshot((snapshot) => {
+      // every time a new post is added, this code fire of
+      setPosts(
+        snapshot.docs.map((doc) => ({
           id: doc.id,
-          post: doc.data()
-        })));
-    })
+          post: doc.data(),
+        }))
+      );
+    });
   }, []);
 
+  const signUp = (event) => {
+    event.preventDefault();
+
+    auth
+    .createUserWithEmailAndPassword(email, password)
+    .catch((error) => alert(error.message))
+  };
 
   return (
     <div className='app'>
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <form className="app__signup">
+            <center>
+              <img
+                className='app__headerImage'
+                src='https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png'
+                alt=''
+              />
+            </center>
+            <Input
+              placeholder='username'
+              type='text'
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Input
+              placeholder='email'
+              type='text'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder='password'
+              type='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button type="submit" onClick={signUp}>Sign Up</Button>
+          </form>
+        </div>
+      </Modal>
       <div className='app__header'>
         <img
           className='app__headerImage'
           src='https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png'
-          alt=""
+          alt=''
         />
       </div>
 
-      {
-        posts.map(({id, post}) => (
-          <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
-        ))
-      }     
+      <Button onClick={() => setOpen(true)}>Sign Up</Button>
 
+      {posts.map(({ id, post }) => (
+        <Post
+          key={id}
+          username={post.username}
+          caption={post.caption}
+          imageUrl={post.imageUrl}
+        />
+      ))}
     </div>
   );
 }
